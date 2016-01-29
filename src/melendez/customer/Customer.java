7dc2a8ceb9d4acb6e0f14a6f2887b8107c50864e
Rolando Melendez <rolando.melendez@utexas.edu>
2016-01-28 22:15:27 -0600

    Probably competed the Customer class. get and sets are done, alter only if we encounter complications.

--- src/melendez/customer/Customer.java
+++ src/melendez/customer/Customer.java
@@ -2,9 +2,72 @@ package melendez.customer;
 
 public class Customer {
 
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+    //Here are the instance variables
+    private String name;
+    private String address;
+    private String city;
+    private String state;
+    private String postalCode;
+
+    //This is the Constructor
+    public Customer() {
+
+        name = "";
+        address = "";
+        city = "";
+        state = "";
+        postalCode = "";
+
+    }
+
+    //Set and get methods for name
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    //Set and get methods for address
+    public void setAddress(String address){
+        this.address = address;
+    }
+    public String getAddress() {
+        return address;
+    }
+
+    //Set and get methods for city
+    public void setCity(String city){
+        this.city = city;
+    }
+    public String getCity() {
+        return city;
+    }
+
+    //Set and get methods for state
+    public void setState(String state){
+        this.state = state;
+    }
+    public String getState() {
+        return state;
+    }
+
+    //Set and get methods for postalCode
+    public void setPostalCode(String postalCode){
+        this.postalCode = postalCode;
+    }
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    //Custom method to get name and address formatted as needed
+    public String getNameAndAddress(){
+        String nameAndAddressFormatted = "";
+        nameAndAddressFormatted = getName() + "\n" +
+                getAddress() + "\n" + getCity() + ", " +
+                getState() + " " + getPostalCode();
+
+        return null;
     }
 
 }
--- src/melendez/customer/CustomerDB.java
+++ src/melendez/customer/CustomerDB.java
@@ -1,10 +1,12 @@
 package melendez.customer;
+import melendez.customer.Customer.*;
 
 public class CustomerDB {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+
+
+
     }
 
 }
